05c4258132c4a1a8b1679b676a90d13d6050ffd7
EvertonKluczkovski <>
2022-02-23 16:27:39 +0000

    - Applying the basic of SRP.

---
+++ SolidPrinciple/JsonPolicySerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace SolidPrinciple
+{
+    public class JsonPolicySerializer
+    {
+        public Policy GetPolicyFromJsonString(string jsonString)
+        {
+            return JsonConvert.DeserializeObject<Policy>(jsonString,
+                new StringEnumConverter());
+        }
+    }
+}
